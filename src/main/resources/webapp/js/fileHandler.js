ada81418fa70fd79ba83cc1d097e1067c8cde42e
oleg.pahhomov@gmail.com <lol98nuj>
2015-09-15 15:21:47 +0300

    fixed reloading and delete buttons

--- src/main/resources/webapp/js/fileHandler.js
+++ src/main/resources/webapp/js/fileHandler.js
@@ -1,35 +1,44 @@
 var fileHandler = {
 
-
     loadPictures: function loadFiles() {
         var showFilesElem = $("#show_files");
-        $.ajax(serverConfig.url('files'))
+        return $.ajax(serverConfig.url('files'))
             .done(function (response) {
-                showFilesElem.html(tmpl("show_files_template",
-                    {
-                        files: response,
-
-                        ratioClass: function (image) {
-                            var experimental_ratio = 1.45;
-                            return image.ratio > experimental_ratio ? 'file bigfile' : 'file';
-                        },
-                        getHref: function (image) {
-                            return "#show_popup_link_" + image.id;
-                        },
-                        imgUrl: function (image) {
-                            return serverConfig.url('picture/'+ image.id);
-                        },
-                        deleteId: function (image) {
-                            return 'delete_file_' + image.id;
-                        }
-
-                    }
-                ))
+                showFilesElem
+                    .html(tmpl("show_files_template", fileHandler.createResponse(response)))
+                    .promise()
+                    .done(function () {
+                        fileHandler.deletePicture();
+                    });
             }).fail(function (jqXHR, textStatus) {
                 showFilesElem.html("Error occurred");
                 console.log("Request failed: " + textStatus);
             });
     },
+
+    createResponse: function (response) {
+        return {
+            files: response,
+
+            ratioClass: function (image) {
+                var experimental_ratio = 1.45;
+                return image.ratio > experimental_ratio ? 'file bigfile' : 'file';
+            },
+
+            getHref: function (image) {
+                return "#show_popup_link_" + image.id;
+            },
+
+            imgUrl: function (image) {
+                return serverConfig.url('picture/' + image.id);
+            },
+
+            deleteId: function (image) {
+                return 'delete_file_' + image.id;
+            }
+        };
+    },
+
     deletePicture: function deletePicture() {
         $.each($("form[id^=delete_file_]"), function (i, item) {
             var deleteBox = $(item);
